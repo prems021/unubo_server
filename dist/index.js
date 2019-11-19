8a6ce5570a935c32e45c390cb5b3f23d76ea4e22
prems021 <premji.netcom@gmail.com>
2019-11-19 23:44:16 +0530

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require('express');
+const body_parser_1 = require("body-parser");
+
+const post_1 = require("./routes/post");
+var cors = require('cors');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 8081;
+app.use(cors());
+app.use(body_parser_1.json());
+app.use(body_parser_1.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.use("/api/post", post_1.postRouter);
+app.get('/api', function (req, res) {
+    res.send('Hello World, How are you!');
+});
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
